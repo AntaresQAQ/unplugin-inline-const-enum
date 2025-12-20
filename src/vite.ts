e16d48d6eeb09a3494ad9941ff6ee0acc5ca447c
Antares <antares.mi@outlook.com>
2025-12-21 01:38:47 +0800

    update

---
+++ src/vite.ts
@@ -0,0 +1,5 @@
+import { InlineConstEnumPlugin } from ".";
+
+const vite: typeof InlineConstEnumPlugin.vite = InlineConstEnumPlugin.vite;
+export default vite;
+export { vite as ViteInlineConstEnumPlugin };
